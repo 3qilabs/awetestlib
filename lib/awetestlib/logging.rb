454ddd58d034990892e697c5850146198d11cc41
pneve <patrick.neve@wellsfargo.com>
2015-07-09 16:11:26 -0700

    incorporate Anay's fixes to message_to_report and debug_to_report to set correct call list index

--- lib/awetestlib/logging.rb
+++ lib/awetestlib/logging.rb
@@ -243,7 +243,7 @@ module Awetestlib
     def message_to_report(message, wai_lvl = 4)
       scr_lvl = first_script_index
       lvl     = scr_lvl > 0 ? scr_lvl : wai_lvl
-      mark_test_level(message, 0, '', 1, lvl)
+      mark_test_level(message, 0, '', 1, lvl + 1)
       true
     end
 
@@ -252,7 +252,7 @@ module Awetestlib
     def debug_to_report(message, wai_lvl = 4)
       scr_lvl = first_script_index
       lvl     = scr_lvl > 0 ? scr_lvl : wai_lvl
-      mark_test_level("(DEBUG): ", 0, "#{message}", 1, lvl)
+      mark_test_level("(DEBUG): ", 0, "#{message}", 1, lvl + 1)
       true
     end
 
